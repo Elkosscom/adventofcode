8dabf3c3be05c3f8026a685c9ecf0bbfdc37a7b1
Elkosscom <52695010+Elkosscom@users.noreply.github.com>
2019-12-02 22:04:10 +0000

    Completed day 1

--- Day 1/1.py
+++ Day 1/1.py
@@ -1,5 +1,6 @@
+## PART 1
 def calculate_fuel(mass):
-    return int(mass / 3) - 2
+    return mass // 3 - 2
 
 
 assert calculate_fuel(12) == 2
@@ -8,13 +9,38 @@ assert calculate_fuel(1969) == 654
 assert calculate_fuel(100756) == 33583
 
 
-with open("1_input.txt", "r") as f:
-    modules = [line[0:-1] for line in f.readlines()]
+with open(r"Day 1\1_input.txt", "r") as f:
+    modules = [int(line[0:-1]) for line in f.readlines()]
 
-print(modules)
 total = 0
 for module in modules:
-    print(calculate_fuel(int(module)))
-    total += calculate_fuel(int(module))
+    total += calculate_fuel(module)
 
-print(f"Total:{total}")
+print(f"Part 1:{total}")
+
+
+## Part 2
+def calculate_total_fuel(mass: int) -> int:
+    output = 0
+    temp = mass
+    while True:
+
+        temp = calculate_fuel(temp)
+        if temp <= 0:
+            break
+        output += temp
+        temp = temp
+    return output
+
+
+assert calculate_total_fuel(1969) == 966
+assert calculate_total_fuel(100756) == 50346
+
+
+total_fuel = 0
+for mass in modules:
+    total_fuel += calculate_total_fuel(mass)
+
+
+print(f"Part 2: {total_fuel}")
+
