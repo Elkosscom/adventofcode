82934aef4111f3d0276b1dd4051521497146dc3e
elkosscom <elkosscombine@gmail.com>
2019-12-06 16:46:55 +0000

    Downloading of input

--- input_dl.py
+++ input_dl.py
@@ -1,3 +1,34 @@
 import requests
-uri = f'http://adventofcode.com/{year}/day/{day}/input'
-response = requests.get(uri, cookies={'session': SESSIONID}, headers={'User-Agent': USER_AGENT})
+import os
+
+with open("sessionid", "rb") as f:
+    SESSIONID = f.read().decode("utf8")
+
+
+def get_response(day):
+    year = 2019
+    uri = f"http://adventofcode.com/{year}/day/{day}/input"
+    response = requests.get(uri, cookies={"session": SESSIONID})
+    return response.content
+
+
+def main():
+    try:
+        day = int(input("Please input day:\n"))
+    except ValueError:
+        print("Please input a number")
+        exit(1)
+
+    folder = f"Day {day}"
+    if folder in os.listdir(os.getcwd()):
+        print("Data already exists")
+    else:
+        try:
+            os.mkdir(folder)
+        finally:
+            with open(f"{folder}\\input", "wb") as f:
+                f.write(get_response(day))
+
+
+if __name__ == "__main__":
+    main()
